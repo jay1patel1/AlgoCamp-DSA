ac530d2972c7abb4fc9e592b911542fd3023321e
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-06-09 12:21:44 +0530

    Updated

--- 01_Array/01. 01 Sorting.js
+++ 01_Array/01. 01 Sorting.js
@@ -2,12 +2,14 @@ function sorting (arr) {
     let point_A = 0;
     let point_B = arr.length-1;
 
-    while (point_A < point_B) {
+    while (point_A <= point_B) {
         if (arr[point_A] === 1) {
             [arr[point_A],arr[point_B]] = [arr[point_B],arr[point_A]];
             point_B--;
-        }
             point_A++;
+        } else if (arr[point_A] === 0) {
+            point_A++;
+        }
     }
     return arr
 }
