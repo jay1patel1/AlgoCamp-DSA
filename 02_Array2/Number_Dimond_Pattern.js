1c0c178dcb138a2d7b2286c3ee8bf34ee8f13800
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-06-15 17:20:11 +0530

    Updated

---
+++ 02_Array2/Number_Dimond_Pattern.js
@@ -0,0 +1,30 @@
+const dimondNumberPrint = (num) => {
+    for (let i = 0; i < num; i++) {
+        let str = "";
+        for (let j = 0; j < i; j++) {
+            str += " ";
+        }
+        for (let j = i + 1; j < num + 1; j++) {
+            str += j + " ";
+        }
+
+        console.log(str);
+    }
+
+    for (let i = 1; i < num; i++) {
+        let str = "";
+        for (let j = 1; j < num-i; j++) {
+            str += " ";
+        }
+        for (let j = num-i; j < num + 1; j++) {
+            str += j + " "
+        }
+
+        console.log(str);
+    }
+}
+
+dimondNumberPrint(6)
+dimondNumberPrint(3)
+dimondNumberPrint(2)
+dimondNumberPrint(25)
