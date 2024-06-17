9a0ed1fa7383fcfa470326de21da27fd65f90387
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-06-17 12:19:19 +0530

    updated

--- 03_2D_Array/src/Printin_2D_Array.js
+++ 03_2D_Array/src/Printin_2D_Array.js
@@ -33,5 +33,32 @@ function wave_Grid(row,col) {
     return ans;
 }
 
+function wave_Grid_Reverse(row,col) {
+    let ans = Array(row);
+    for (let i = 0; i < ans.length; i++) {
+        ans[i] = Array(col);
+    }
+
+    let num = 0;
+
+    for (let i = 0; i < col; i++) {
+        if (i%2 === 0) {
+            for (let j = row-1; j >= 0; j--) {
+                num++;
+                ans[j][i] = num;
+            }
+        } else {
+            for (let j = 0; j < row; j++) {
+                num++;
+                ans[j][i] = num;
+            }
+        }
+    }
+
+    console.log(ans);
+    return ans;
+}
+
 printIn_2D_Array(3,2);
 wave_Grid(3,2);
+wave_Grid_Reverse(4,5);
