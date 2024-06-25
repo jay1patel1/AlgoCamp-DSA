090b1f547ba7a58966f74ed60e31f236e89a4776
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-06-25 21:43:22 +0530

    updated

--- 03_2D_Array/src/ReshapMatrix.js
+++ 03_2D_Array/src/ReshapMatrix.js
@@ -8,9 +8,27 @@ const matrixReshape = function(mat, r, c) {
             ansArray.push(Array(c).fill(0));
         }
 
+        let currentRow = 0;
+        let currentCol = 0;
+
+        for (let i = 0; i < mat.length; i++) {
+            for (let j = 0; j < mat[0].length; j++) {
+                ansArray[currentRow][currentCol] = mat[i][j];
+                currentCol++;
+                if (currentCol > (c-1)) {
+                    currentRow++;
+                    currentCol = 0;
+                    // if (currentRow >= (r-1)) {
+                    //     return ansArray;
+                    // }
+                }
+            }
+        }
+
         return ansArray;
     }
 };
 
 console.log(matrixReshape([[1,2],[3,4]],1,4))
 console.log(matrixReshape([[1,2],[3,4]],2,4))
+console.log(matrixReshape([[1,2],[3,4]],4,1))
