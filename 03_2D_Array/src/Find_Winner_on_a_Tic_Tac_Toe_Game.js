c203cb9a7451ae890f765384b42b52910ce8ceb8
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-06-26 08:17:12 +0530

    updated

--- 03_2D_Array/src/Find_Winner_on_a_Tic_Tac_Toe_Game.js
+++ 03_2D_Array/src/Find_Winner_on_a_Tic_Tac_Toe_Game.js
@@ -3,8 +3,26 @@
  * @return {string}
  */
 const tictactoe = function(moves) {
+    let turn = 0;
+    let arr = Array(9).fill(-1);
 
+    for (let i = 0; i < moves.length; i++) {
+        let x = moves[i][0];
+        let y = moves[i][1];
+
+        if (i%2 === 0) {
+            arr[(x*3) + y] = "X";
+        } else {
+            arr[(x*3) + y] = "0";
+        }
+    }
+
+    return winnerAlgo(arr);
 };
+
+function winnerAlgo (grid) {
+
+}
 
 console.log(tictactoe([[0,0],[2,0],[1,1],[2,1],[2,2]]))
 console.log(tictactoe([[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]))
